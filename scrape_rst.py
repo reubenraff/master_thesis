6eb709df3c211ba9b039f530b81dccf4193a5c1e
Reuben Raff <reubenraff@gmail.com>
2022-03-27 19:04:52 -0700

    Create scrape_rst.py

---
+++ scrape_rst.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup
+from typing import Any, dict
+
+def scrape_rst_files(file: Any) --> dict:
+    with open("abnb_lots_of_labels.xml","r") as rst, open("rel_dump.rst", "w") as sink:
+        contents = rst.read()
+        soup = BeautifulSoup(contents, "lxml")
+        relation_tag = soup.find("segment")
+        relation_tags = soup.find_all("segment") #tags
+        data_dict = {}
+        for relation_tag in relation_tags:
+        #print(relation_tag)
+            if len(relation_tag.attrs) == 3:
+                data_dict[relation_tag["relname"]] = relation_tag.text
+    return(data_dict)
